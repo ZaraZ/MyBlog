7fe3ff050e8d47b15c1b6d5aa706b525f7960afa
Abracadabra <registersth@outlook.com>
2016-12-08 11:34:23 +0800

    留言基本功能完成

--- web/www/msg_add.php
+++ web/www/msg_add.php
@@ -9,5 +9,34 @@
 * @copyright GPL
 *
 */
-  include('./view/msg_add.html');
+
+if (empty($_POST)) {
+    include('./view/msg_add.html');     //若没有输入，点击提交还是引入当前页面
+} else {
+    //连接数据库，并把留言内容赋值给 $msgcontent
+    $msgcontent = trim($_POST["msg_content"]);    //用trim( )函数来移除提交的留言首位的空白字符
+    $host = '127.0.0.1';
+    $root = 'root';
+    $pwd = 'admin';
+    $conn = mysql_connect($host, $root, $pwd);
+    mysql_select_db("my_blog", $conn);  //选择数据库
+    mysql_query("set names 'utf8'"); //设定字符集
+
+    //检测留言是否为空
+    if (empty($msgcontent)) {
+        echo '留言不能为空';
+        exit();
+    }else {   //将留言内容写入数据库
+      $sql_insert = "insert into msg (msg_content) values('$msgcontent')";
+      $res_insert = mysql_query($sql_insert);
+      if ($res_insert) {
+        echo '留言成功！';
+        exit();
+      }else {
+        echo '系统繁忙，请稍候！';
+        exit();
+      }
+    }
+}
+
 ?>
