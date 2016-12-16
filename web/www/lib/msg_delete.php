61fec6206b76d560742baa2df93d85c1ebdc129c
Abracadabra <registersth@outlook.com>
2016-12-16 13:33:29 +0800

    留言删除功能

---
+++ web/www/lib/msg_delete.php
@@ -0,0 +1,42 @@
+<meta charset="utf8">
+<?php
+/**
+* msg_delete.php 留言删除
+*
+* @author Abra <xiangyunz@outlook.com> //作者邮箱
+* @link https://github.com/ZaraZ       //作者的github
+* @since 0.1 2016年12月16日             //版本号及日期
+* @copyright GPL
+*
+*/
+
+//msg_id
+$msg_id = $_GET['msg_id'];
+
+//连接数据库，并把留言内容赋值给 $msgcontent
+$host = '127.0.0.1';
+$root = 'root';
+$pwd = 'admin';
+$conn = mysql_connect($host, $root, $pwd);
+mysql_select_db("my_blog", $conn);  //选择数据库
+mysql_query("set names 'utf8'"); //设定字符集
+
+if (!is_numeric($msg_id)) {
+  echo "<script>alert('ERRO！请检查留言地址！'); history.go(-1);</script>";
+}else {
+  $sql = "select msg_id from msg where msg_id = '$msg_id'";
+  $sql_rel = mysql_query($sql);
+  $num = mysql_num_rows($sql_rel);
+  if (!$num) {
+    echo "<script>alert('不存在该留言'); history.go(-1);</script>";
+  }else {
+    $msg_del = mysql_query("delete from msg where msg_id = '$msg_id'");
+    if (!$msg_del) {
+      echo "<script>alert('留言删除失败'); history.go(-1);</script>";
+      }else {
+        echo "<script>alert('留言删除成功！');history.go(-1);</script>";
+      }
+  }
+}
+
+?>
