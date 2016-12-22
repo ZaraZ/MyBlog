fe43e8cf15f3431d741c25620220eea5e425eeed
Abracadabra <registersth@outlook.com>
2016-12-22 17:07:38 +0800

    初始化文件

---
+++ web/www/lib/init.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* init.php 初始化文件
+*
+* @author Abra <xiangyunz@outlook.com> //作者邮箱
+* @link https://github.com/ZaraZ       //作者的github
+* @since 0.1 2016年12月22日             //版本号及日期
+* @copyright GPL
+*
+*/
+
+header('Content-type:text/html;charset=utf8');
+define('www', dirname(__DIR__));
+// echo www;
+require(www . '/lib/mysql.php');
+
+ ?>
