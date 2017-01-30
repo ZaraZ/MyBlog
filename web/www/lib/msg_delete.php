d8f5cfb1bbcce304e81f6203827271ad86443bb3
Abracadabra Z <Abracadabra Z>
2017-01-30 15:13:17 +0800

    留言删除代码更改

--- web/www/lib/msg_delete.php
+++ web/www/lib/msg_delete.php
@@ -10,27 +10,29 @@
 *
 */
 
+require('./init.php');
+
 //msg_id
 $msg_id = $_GET['msg_id'];
 
-//连接数据库，并把留言内容赋值给 $msgcontent
-$host = '127.0.0.1';
-$root = 'root';
-$pwd = 'admin';
-$conn = mysql_connect($host, $root, $pwd);
-mysql_select_db("my_blog", $conn);  //选择数据库
-mysql_query("set names 'utf8'"); //设定字符集
+// //连接数据库，并把留言内容赋值给 $msgcontent
+// $host = '127.0.0.1';
+// $root = 'root';
+// $pwd = 'admin';
+// $conn = mysql_connect($host, $root, $pwd);
+// mysql_select_db("my_blog", $conn);  //选择数据库
+// mysql_query("set names 'utf8'"); //设定字符集
 
 if (!is_numeric($msg_id)) {
   echo "<script>alert('ERRO！请检查留言地址！'); window.location.assign('../msg.php');</script>";
 }else {
   $sql = "select msg_id from msg where msg_id = '$msg_id'";
-  $sql_rel = mysql_query($sql);
-  $num = mysql_num_rows($sql_rel);
+  $sql_rel = mQuery($sql);
+  $num = mGetRow($sql_rel);
   if (!$num) {
     echo "<script>alert('不存在该留言'); window.location.assign('../msg.php');</script>";
   }else {
-    $msg_del = mysql_query("delete from msg where msg_id = '$msg_id'");
+    $msg_del = mQuery("delete from msg where msg_id = '$msg_id'");
     if (!$msg_del) {
       echo "<script>alert('留言删除失败');window.location.assign('../msg.php');</script>";
       }else {
