1a96c3886b5b41b548401bbf328d549c74037aaa
Abracadabra <registersth@outlook.com>
2017-02-05 23:06:11 +0800

    修改注册方法，注册成功后直接跳转到登陆页面

--- web/www/sign_up_check.php
+++ web/www/sign_up_check.php
@@ -9,31 +9,36 @@
 * @copyright GPL
 *
 */
+
+require('./lib/init.php');
+
 if (isset($_POST["submit"]) && $_POST["submit"] == "注册") {
     $user = $_POST["user_name"];
     $psw = $_POST["psw"];
     $psw_confirm = $_POST["confirm"];
-    $host = '127.0.0.1';
-    $root = 'root';
-    $pwd = 'admin';
+    // $host = '127.0.0.1';
+    // $root = 'root';
+    // $pwd = 'admin';
     if ($user == "" || $psw == "" || $psw_confirm == "") {
         echo "<script>alert('请确认信息完整性！'); history.go(-1);</script>";
     } else {
         if ($psw == $psw_confirm) {
-            $con = mysql_connect($host, $root, $pwd);   //连接数据库
-             mysql_select_db("my_blog");  //选择数据库
-             mysql_query("set names 'utf8'"); //设定字符集
+            // $con = mysql_connect($host, $root, $pwd);   //连接数据库
+            //  mysql_select_db("my_blog");  //选择数据库
+            //  mysql_query("set names 'utf8'"); //设定字符集
              $sql = "select user_name from users where user_name = '$user'"; //SQL语句
-             $result = mysql_query($sql);    //执行SQL语句
-             $num = mysql_num_rows($result); //统计执行结果影响的行数
+            //  $result = mysql_query($sql);    //执行SQL语句
+             $num = mysql_num_rows(mQuery($sql)); //统计执行结果影响的行数
              if ($num) {    //如果已经存在该用户
                  echo "<script>alert('用户名已存在'); history.go(-1);</script>";
-             } else {    //不存在当前注册用户名称
-                 $sql_insert = "insert into users (user_name,psw) values('$user','$psw')";
-                 $res_insert = mysql_query($sql_insert);
+             } else {    //若不存在当前注册用户名称
+                $data = array('user_name' => $user, 'psw' => $psw);
+                //  $sql_insert = "insert into users (user_name,psw) values('$user','$psw')";
+                //  $res_insert = mysql_query($sql_insert);
+                $res_insert = mExec('users', $data);
                  //$num_insert = mysql_num_rows($res_insert);
                  if ($res_insert) {
-                     echo "<script>alert('注册成功！'); history.go(-1);</script>";
+                     echo "<script>alert('注册成功！');location.assign('./sign_in.php');</script>";
                  } else {
                      echo "<script>alert('系统繁忙，请稍候！'); history.go(-1);</script>";
                  }
