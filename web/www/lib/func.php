8664479f5f1cb358790896771ecbc137e99b9ecf
Abracadabra <registersth@outlook.com>
2017-02-05 18:13:29 +0800

    登陆后写入cookie

--- web/www/lib/func.php
+++ web/www/lib/func.php
@@ -17,13 +17,22 @@
 * 获取当前北京时间
 * @return 当前北京时间
 */
-function mTime(){
+function fTime(){
   date_default_timezone_set('Asia/Shanghai');
   $t = time();
   return date("Y-m-d H:i:s", $t);
 }
 
-// $t = mTime();
+// $t = fTime();
 // echo $t;
+
+/**
+*
+* 检测是否登录
+* @return bool 是否存在 name 的cookie
+*/
+function flogin(){
+  return isset($_COOKIE['name']);
+}
 
 ?>
--- web/www/lib/sign_in_check.php
+++ web/www/lib/sign_in_check.php
@@ -10,23 +10,28 @@
 *
 */
 
+require('./init.php');
+
+
 if (isset($_POST["submit"]) && $_POST["submit"] == "登陆") {
     $user = $_POST["user_name"];
     $psw = $_POST["psw"];
-    $host = '127.0.0.1';
-    $root = 'root';
-    $pwd = 'admin';
+    // $host = '127.0.0.1';
+    // $root = 'root';
+    // $pwd = 'admin';
     if ($user == "" || $psw == "") {
         echo "<script>alert('请输入用户名或密码！'); history.go(-1);</script>";
     } else {
-        $con = mysql_connect($host, $root, $pwd);   //连接数据库
-       mysql_select_db("my_blog");  //选择数据库
-       mysql_query("set names 'utf8'"); //设定字符集
+      //   $con = mysql_connect($host, $root, $pwd);   //连接数据库
+      //  mysql_select_db("my_blog");  //选择数据库
+      //  mysql_query("set names 'utf8'"); //设定字符集
        $sql = "select user_name, psw from users where user_name = '$user' and psw = '$psw'"; //SQL语句
-       $result = mysql_query($sql);    //执行SQL语句
-       $num = mysql_num_rows($result); //统计执行结果影响的行数
+      //  $result = mysql_query($sql);    //执行SQL语句
+       $num = mGetRow($sql); //统计执行结果影响的行数
        if ($num) {    //匹配到用户名及密码
-           echo "<script>alert('登陆成功'); history.go(-1);</script>";
+          echo "<script>alert('登陆成功');</script>";
+          setcookie('name', $num['user_name']);
+          header('Location:../index.php');
        } else {    //没有匹配成功
                  echo "<script>alert('用户名或密码不正确！');history.go(-1);</script>";
              }
