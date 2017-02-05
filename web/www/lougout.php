6f685193f3da7bbaacdd46792bd0ec35dc8bad35
Abracadabra <registersth@outlook.com>
2017-02-05 18:48:20 +0800

    登陆后页面以及用户登出

---
+++ web/www/lougout.php
@@ -0,0 +1,20 @@
+<meta charset="utf8">
+<?php
+/**
+* lougout.php 用户登出页
+*
+* @author Abra <xiangyunz@outlook.com> //作者邮箱
+* @link https://github.com/ZaraZ       //作者的github
+* @since 0.1 2017年02月05日             //版本号及日期
+* @copyright GPL
+*
+*/
+  require('./lib/init.php');
+  
+  if (!fLogin()) {
+    header('Location:sign_in.php');
+  }else {
+    setcookie('name', null, 0);
+    header('Location:index.php');
+  }
+ ?>
